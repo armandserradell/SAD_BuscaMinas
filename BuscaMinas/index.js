3582db11094e48452cdbafe42546b7e8c23a03f1
armandserradell <160116937+armandserradell@users.noreply.github.com>
2024-05-03 13:17:02 +0200

    index.js

--- BuscaMinas/index.js
+++ BuscaMinas/index.js
@@ -4,7 +4,7 @@ let lado = 40
 
 let marcas = 0
 
-let minas = columnas * filas * 0.1
+let minas = columnas * filas * 0.15
 
 let tablero = []
 let enJuego = true
@@ -28,6 +28,7 @@ function generarTableroHTML() {
             html += `<td id="celda-${c}-${f}" style="width:${lado}px;height:${lado}px"`
             // html += `${c}-${f}`
             html += `</td>`
+           
 
         }
         html += `</tr>`
@@ -38,6 +39,21 @@ function generarTableroHTML() {
     tableroHTML.innerHTML = html
     tableroHTML.style.width = columnas * lado + "px"
     tableroHTML.style.height = filas * lado + "px"
+
+   /* for (let f = 0; f < filas; f++) {
+        for (let c = 0; c < columnas; c++) {
+            let celda = document.getElementById(`celda-${c}-${f}`)
+            if(celda.estado == undefined && (c+f)%2==0){
+                celda.style.background = "#7DCEA0"
+            }else{
+                celda.style.background = "#A9DFBF"
+
+            }
+        }
+    }
+
+*/
+   
 
 }
 
@@ -95,12 +111,21 @@ function refrescarTablero() { //Estado visual tablero
             //celda.innerHTML = tablero[c][f].valor
 
             if (tablero[c][f].estado == "descubierto") {
-                celda.style.background = "rgb(206, 146, 146)"
+                celda .classList.add("tierra")
                celda.style.boxShadow = "none"
+              /* if((c+f)%2 == 0){
+                celda.style.background = "#F5CBA7"
+               }else{
+                celda.style.background = "#FDEBD0"
+                
+               }*/
+
                 
                 switch (tablero[c][f].valor) {
                     case -1:
-                        celda.innerHTML = "B"
+                       celda.innerHTML = "&#128163" //bomba , diana 127919
+                       //celda.innerHTML = '<img src="img/bomba.png" style="width: 100%; height: 100%; object-fit: cover; ">';
+                       celda.style.background = "white" 
                         break;
 
 
@@ -111,10 +136,21 @@ function refrescarTablero() { //Estado visual tablero
                         break;
                 }
             }
+            if(tablero[c][f].estado == "marcado"){
+                celda.innerHTML = "&#127919" //diana
+                celda.style.background = "lightgreen"
+
+            }
+            if(tablero[c][f].estado == undefined){
+                celda.innerHTML= ""
+                celda.style.background = ""
+            }
 
         }
     }
-}
+    verificarGanador()
+    verificarPerdedor()
+} 
 
 function generarTableroJuego() {
     vaciarTablero()
@@ -171,6 +207,42 @@ function contadoresMinas() {
         }
 
     }
+}
+
+function verificarGanador(){
+    
+   
+
+}
+
+function verificarPerdedor(){
+    for (let f = 0; f < filas; f++) {
+        for (let c = 0; c < columnas; c++) {
+            if(tablero[c][f].valor == -1 && tablero[c][f].estado == "descubierto"){
+                let explosion = document.getElementById("explosion")
+                explosion.style.display = "block"
+                setTimeout(function() {
+                    explosion.style.display = "none";
+                }, 2500); // ms
+                enJuego = false
+
+            }
+        }
+    } //Mostrar todas las minas
+    if(enJuego){
+        return
+    }
+    for (let f = 0; f < filas; f++) {
+        for (let c = 0; c < columnas; c++) {
+            if(tablero[c][f].valor == -1){
+                let celda = document.getElementById(`celda-${c}-${f}`)
+                celda.innerHTML = "&#128163"
+                celda.style.color = "black"
+                
+            }
+        }
+    }
+
 }
 
 
